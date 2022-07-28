e887e84714c0a0b0a3f6504c4f6527b9a3e49878
stern <88396589+sternladung@users.noreply.github.com>
2022-07-28 13:11:10 +0300

    Update amanda.lua

--- amanda.lua
+++ amanda.lua
@@ -8,8 +8,8 @@ local repst = game:GetService("ReplicatedStorage");
 
 plr.Character.Humanoid.HealthChanged:Connect(function(newhp)
     local totalhp = math.abs(plr.Character.Humanoid.Health - newhp)
+    local events = repst.Events;
     if totalhp == 0 then
-        local events = repst.Events;
         wait(3)
         events.revv:FireServer()
     end
