9a7453192f0f01542aecd40e9cf53127a90e5162
stern <88396589+sternladung@users.noreply.github.com>
2022-08-05 21:32:29 +0300

    Update binary.lua

--- binary.lua
+++ binary.lua
@@ -2,22 +2,24 @@ local binary = {}
 
 function binary:ToBinary(toBinary)
     local binaryString = {}
+
     for _,v in ipairs(toBinary:split'') do
-        local binary = ""
+        local bbinary = ""
         local byte = v:byte()
         while byte > 0 do
-            binary = tostring(byte % 2) .. binary
+            bbinary = tostring(byte % 2) .. bbinary
             byte = math.modf(byte / 2)
         end
-        table.insert(binaryString, string.format("%.8d", binary))
+        table.insert(binaryString, string.format("%.8d", bbinary))
     end
 
     return table.concat(binaryString, "")
 end
 
-function binary:ToString(toString)
+function binary:ToString(binaryString)
     local theString = ""
-    for _,v in ipairs(toString:split' ') do
+
+    for _,v in ipairs(BinaryString:split' ') do --> ex: {"01000001", "01000010", "01000011"}
         local byte = tonumber(v, 2)
         theString ..= string.char(byte)
     end
