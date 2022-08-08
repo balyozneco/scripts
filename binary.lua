b6d3440384f0cbe15b345684b53dbaa7f2f3e47d
stern <88396589+sternladung@users.noreply.github.com>
2022-08-08 14:20:15 +0300

    Update binary.lua

--- binary.lua
+++ binary.lua
@@ -19,7 +19,7 @@ end
 function binary:ToString(binaryString)
     local theString = ""
 
-    for _,v in ipairs(BinaryString:split' ') do --> ex: {"01000001", "01000010", "01000011"}
+    for _,v in ipairs(binaryString:split' ') do --> ex: {"01000001", "01000010", "01000011"}
         local byte = tonumber(v, 2)
         theString ..= string.char(byte)
     end
