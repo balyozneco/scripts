0f22840b58a51af527a5b0badbed1873dfc7005c
stern <88396589+sternladung@users.noreply.github.com>
2022-08-08 14:28:59 +0300

    Update binary.lua

--- binary.lua
+++ binary.lua
@@ -16,15 +16,15 @@ function binary:ToBinary(toBinary)
     return table.concat(binaryString, "")
 end
 
-function binary:ToString(binaryString)
-    local theString = ""
-
-    for _,v in ipairs(binaryString:split' ') do --> ex: {"01000001", "01000010", "01000011"}
-        local byte = tonumber(v, 2)
-        theString ..= string.char(byte)
-    end
-
-    return theString
+function binaryToString(BinaryString)
+	local String = ""
+	
+	for i, Binary in ipairs(BinaryString:split' ') do --> ex: {"01000001", "01000010", "01000011"}
+		local Byte = tonumber(Binary, 2) -- convert binary (base 2) to ascii character code
+		String ..= string.char(Byte) -- get character from ascii code and append it at end of string
+	end
+	
+	return String
 end
 
 return binary
